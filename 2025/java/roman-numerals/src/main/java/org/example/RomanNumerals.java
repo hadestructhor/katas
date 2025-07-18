32ed316b14870c5b3ee350e1f117dc528cb2f306
hadestructhor <60148800+hadestructhor@users.noreply.github.com>
2025-07-19 00:16:05 +0200

    feat(java/roman-numerals): solving case for 5

--- 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
+++ 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
@@ -5,6 +5,9 @@ public class RomanNumerals {
         if(number == 4) {
             return RomanLetter.I.getRepresentation() + RomanLetter.V.getRepresentation();
         }
+        if(number == 5) {
+            return RomanLetter.V.getRepresentation();
+        }
         return RomanLetter.I.getRepresentation().repeat(number);
     }
 }
