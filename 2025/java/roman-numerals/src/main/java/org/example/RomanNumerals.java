06e44d6d580521feab2a11988c6806fe79057ee1
hadestructhor <60148800+hadestructhor@users.noreply.github.com>
2025-07-19 00:06:56 +0200

    feat(java/roman-numerals): solving case for 3

--- 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
+++ 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class RomanNumerals {
     public static String fromDecimal(int i) {
-        return RomanLetter.I.getRepresentation();
+        return RomanLetter.I.getRepresentation().repeat(i);
     }
 }
