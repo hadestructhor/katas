489a33703b2731b3680175d0b2b536371ef7ad2e
hadestructhor <60148800+hadestructhor@users.noreply.github.com>
2025-07-19 00:08:07 +0200

    refactor(java/roman-numerals): replaced i with decimal

--- 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
+++ 2025/java/roman-numerals/src/main/java/org/example/RomanNumerals.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class RomanNumerals {
-    public static String fromDecimal(int i) {
-        return RomanLetter.I.getRepresentation().repeat(i);
+    public static String fromDecimal(int number) {
+        return RomanLetter.I.getRepresentation().repeat(number);
     }
 }
